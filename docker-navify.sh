6bf5782e9f55a8e95f8d4db8c9bba30feadc1e95
root <root@a34mmvl003.devillo.no>
2015-07-06 08:11:34 +0200

    moved around some stuff

---
+++ docker-navify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IMAGE=$1
+
+echo "Pulling image $IMAGE from central registry"
+docker pull $IMAGE
+
+REPO_AND_VERSION=$(echo $IMAGE | cut -d"/" -f2)
+NEW_NAME="localhost:5000/$REPO_AND_VERSION"
+
+echo "Tagging with new local name $NEW_NAME"
+
+docker tag -f $IMAGE $NEW_NAME
+
+echo "Pushing image $NEW_IMAGE to internal registry (docker.adeo.no:5000)"
+docker push $NEW_NAME
+echo "Done! Image should now be pullable with docker.adeo.no:5000/$NEW_NAME"
+
