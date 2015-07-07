4027d46c39fa51c92a401cbd198c72622007101c
root <root@a34mmvl003.devillo.no>
2015-07-07 09:21:47 +0200

    suksess bare hvis

--- docker-navify.sh
+++ docker-navify.sh
@@ -13,6 +13,5 @@ echo "Tagging with new local name $NEW_NAME"
 docker tag -f $IMAGE $NEW_NAME
 
 echo "Pushing image $NEW_IMAGE to internal registry (docker.adeo.no:5000)"
-docker push $NEW_NAME
-echo "Done! Image should now be pullable with docker.adeo.no:5000/$REPO_AND_VERSION"
+docker push $NEW_NAME && echo "Done! Image should now be pullable with docker.adeo.no:5000/$REPO_AND_VERSION"
 
