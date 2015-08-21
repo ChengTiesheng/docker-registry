0e98c309ecd4adbb908a287c76ecbbff7c9d8d41
Johnny Horvi <johnny.horvi@gmail.com>
2015-08-21 09:57:18 +0200

    Update start_docker-registry_container.sh

--- start_docker-registry_container.sh
+++ start_docker-registry_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 5000:5000 -v /var/docker-registry:/var/docker-registry --restart="always" secure_registry:2.1.1
+docker run -d -p 5000:5000 -v /var/docker-registry:/var/docker-registry --restart="always" <MY_IMAGE_NAME>:<MY_VERSION>
